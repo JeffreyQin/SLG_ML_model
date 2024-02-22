9bed0e5417bdcd7260c3735d8ffbf78fa78c46b0
Jeffrey Qin <jeffreyqin612@gmail.com>
2024-02-22 17:31:53 -0500

    removed session based data reformatting

--- format_dataset.py
+++ format_dataset.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 
 
 def preproc_label(raw_label_file):
-    return ''
     with open(raw_label_file, 'r') as f:
         label = f.read()
         label = label.lower()
@@ -19,30 +18,31 @@ def format_dataset(raw_folder='raw_data', formatted_folder='motion_data'):
     if not os.path.isdir(formatted_folder):
         os.mkdir(formatted_folder)
 
+    data_idx = 0
+
     for sess_idx, sess in tqdm(enumerate(os.listdir(raw_folder))):
         sess_folder = os.path.join(raw_folder, sess)
-        formatted_sess_folder = os.path.join(formatted_folder, str(sess_idx) + '_session')
-        if not os.path.isdir(formatted_sess_folder):
-            os.mkdir(formatted_sess_folder)
         for example_idx, example in enumerate(os.listdir(sess_folder)):
             input_file = os.path.join(sess_folder, example, 'data.csv')
             label_file = os.path.join(sess_folder, example, 'text.txt')
 
             input_data = np.genfromtxt(input_file, delimiter=',', usecols=tuple(range(1,9)))
-            np.save(os.path.join(formatted_sess_folder, str(example_idx) + '.npy'), input_data)
+            np.save(os.path.join(formatted_folder, str(data_idx) + '.npy'), input_data)
 
             processed_label = preproc_label(label_file)
-            with open(os.path.join(formatted_sess_folder, str(example_idx) + '.txt'), 'w') as f:
+            with open(os.path.join(formatted_folder, str(data_idx) + '.txt'), 'w') as f:
                 f.write(processed_label)
-            
-            with open(os.path.join(formatted_sess_folder, str(example_idx) + '.json'), 'w') as f:
+
+            with open(os.path.join(formatted_folder, str(data_idx) + '.json'), 'w') as f:
                 data = {
+                    "index": data_idx,
                     "session": sess_idx,
                     "example": example_idx,
                     "label": processed_label
                 }
                 json.dump(data, f)
-
+            
+            data_idx += 1
 
 
 if __name__ == '__main__':
--- generate_split.py
+++ generate_split.py
@@ -3,36 +3,33 @@ import json
 import random
 
 
-def generate_split(split: list[int], parent_folder='motion_data'):
+def generate_split(split: list[int], folder='motion_data'):
     
     trainset, testset, valset = [], [], []
 
-    for sess in os.listdir(parent_folder):
-        sess_folder = os.path.join(parent_folder, sess)
-        sess_idx = sess[0]
-        for file in os.listdir(sess_folder):
-            if file.endswith('.json'):
-                example_idx = file[0]
+    for file in os.listdir(folder):
+        if file.endswith('.json'):
+            data_idx = int(file[0])
 
-                category = random.choices(['train', 'val', 'test'], split)[0]
-                if category == 'train':
-                    trainset.append([sess_idx, example_idx])
-                elif category == 'val':
-                    valset.append([sess_idx, example_idx])
-                else:
-                    testset.append([sess_idx, example_idx])
-                    
+            category = random.choices(['train', 'val', 'test'], split)[0]
+            if category == 'train':
+                trainset.append(data_idx)
+            elif category == 'val':
+                valset.append(data_idx)
+            else:
+                testset.append(data_idx)
+    
     random.shuffle(trainset)
     random.shuffle(valset)
     random.shuffle(testset)
 
     with open('dataset_split.json', 'w') as f:
         dataset_split = { "train": trainset, "val": valset, "test": testset }
-        json.load(dataset_split, f)
+        json.dump(dataset_split, f)
     
                 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     with open('config.json', 'r') as config_file:
         config = json.load(config_file)
         generate_split(config['train_val_test_split'])
