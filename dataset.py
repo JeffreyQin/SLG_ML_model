acd73fab2b9298dd304a289eb1d260b142f372bd
Jeffrey Qin <122770444+JeffreyQin@users.noreply.github.com>
2024-02-26 20:08:31 -0500

    Updated dataset.py

--- dataset.py
+++ dataset.py
@@ -9,9 +9,9 @@ import os, sys, json
 
 
 class MotionDataset(utils.data.Dataset):
-    def __init__(self, test=False, val=False, token_type='char', data_folder='motion_data'):
+    def __init__(self, test=False, val=False, tokenizer):
         
-        self.tokenizer = Tokenizer()
+        self.tokenizer = tokenizer
 
         with open('dataset_split.json', 'r') as dataset_split:
             data = json.load(dataset_split)
@@ -22,30 +22,21 @@ class MotionDataset(utils.data.Dataset):
             else:
                 self.indices = data['val']
 
-        inputs, labels = [], []
+        inputs = []
         for idx in self.indices:
             input = np.load(os.path.join(data_folder, str(idx) + '.npy'))
-            with open(os.path.join(data_folder, str(idx) + '.txt'), 'r') as label_file:
-                label = label_file.read()
             inputs.append(input)
-            labels.append(label)
 
         self.X = preproc_timeseries(inputs)
-        if token_type == 'char':
-            embeddings, lengths, vocabs = self.tokenizer.char_tokenize(labels)
-        else:
-            embeddings, lengths, vocabs = self.tokenizer.subword_tokenize(labels)
+        embeddings, self.lengths, self.vocab = self.tokenizer.get_tokenized(self.indices)
         self.Y = nn.utils.rnn.pad_sequence([torch.tensor(embedding) for embedding in embeddings], batch_first=True)
-        # self.Y = nn.utils.rnn.pack_padded_sequence(self.Y, lengths, batch_first=True, enforce_sorted=False)
-        self.vocabs = vocabs
-
-
+        
         self.X, self.Y = torch.tensor(np.array(self.X)), torch.tensor(np.array(self.Y))
         self.X, self.Y = self.X.type(torch.float32), self.Y.type(torch.float32)
 
     def __len__(self):
         return len(self.indices)
 
-    def __getitem__():
-        pass
+    def __getitem__(self, index):
+        return self.X[index], self.Y[index], self.lengths[index]
 
